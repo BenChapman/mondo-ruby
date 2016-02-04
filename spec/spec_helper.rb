1b26b0609801eb03d9b662da1b5a8b1176693a2f
Ben Chapman <hello@benchapman.ie>
2016-02-04 23:14:07 +0000

    Fix spechelper

--- spec/spec_helper.rb
+++ spec/spec_helper.rb
@@ -1,2 +1,7 @@
 require 'active_support/hash_with_indifferent_access'
-require './lib/mondo'
+require 'mocks/client_mock'
+
+require File.expand_path('../../lib/mondo', __FILE__)
+
+RSpec.configure do |c|
+end
